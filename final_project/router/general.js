3876334fbcc12b5b892c43819934ee9b2bb38ed6
Samantha <smherron21@gmail.com>
2025-05-15 17:27:16 -0400

    completed code to search by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,9 +19,17 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  
+    const isbn = req.params.isbn; // Retrieve ISBN from request parameters
+
+    // Assuming `books` is an object where keys are ISBNs and values are book details
+    if (books[isbn]) {
+        res.json(books[isbn]); // Return book details as JSON
+    } else {
+        res.status(404).json({ message: "Book not found" }); // Handle missing books
+    }
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
